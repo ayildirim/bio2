98a60479bd379cda15e15201c7dd479d96cdf780
Ahmet YILDIRIM <yildirimcoder@gmail.com>
2018-11-06 01:15:05 +0100

    updated site

---
+++ build/precache-manifest.8a01fa45aaa5aa768c5bba14335778c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c841c23bd3f332b4ede",
+    "url": "./static/css/main.bafe71d2.chunk.css"
+  },
+  {
+    "revision": "1c841c23bd3f332b4ede",
+    "url": "./static/js/main.1c841c23.chunk.js"
+  },
+  {
+    "revision": "6d708dcb9badae98c4b9",
+    "url": "./static/js/1.6d708dcb.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b45aa886405dfd5267f5a94e1af31367",
+    "url": "./index.html"
+  }
+];
